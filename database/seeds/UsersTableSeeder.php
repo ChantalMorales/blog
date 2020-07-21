6ed136020c10a7f26575e991be3005a31a8cf595
ChantalMorales <chantal.morales@epn.edu.ec>
2020-07-21 09:27:24 -0500

    #12 relaciones

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Category;
 use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -24,11 +26,20 @@ class UsersTableSeeder extends Seeder
             'password'=>$password,
         ]);
         for ($i =0; $i <50; $i++){
-            User::create([
+            $user= User::create([
                 'name' => $faker->name,
                 'email'=>$faker->email,
                 'password'=> $password,
             ]);
+            $user->categories()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Category::find(1),
+                        Category::find(2),
+                        Category::find(3)
+                    ), $faker->numberBetween(1, 3), false
+                )
+            );
         }
     }
 }
